5096a1865463c222873306d1212e5a6b6fcf043a
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-07-11 19:57:25 +0900

    5.20まで学習完了

---
+++ chap05/str_in.py
@@ -0,0 +1,12 @@
+msg = 'WINGSプロジェクト'
+
+print('プロ' in msg)
+print('プロ' not in msg)
+print(msg.startswith('WINGS'))
+print(msg.endswith('WINGS'))
+print(not msg.startswith('WINGS'))
+print(not msg.endswith('WINGS'))
+print(msg.startswith('WINGS', 1))
+print('プロ' in msg[6:])
+print('wings' in msg)
+print('wings' in msg.casefold())
