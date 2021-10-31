4c306158c10ef1567518a7aaa5e63d8767521753
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-10-31 20:29:37 +0900

    6.1.10 リストの内容を並び変える
    学習完了

---
+++ chap06/list_copy.py
@@ -0,0 +1,6 @@
+data = ['鈴木', '田中', '井上', '加藤']
+data2 = data.copy()
+
+print(data2)
+print(data == data2)
+print(data is data2)
---
+++ chap06/list_sort.py
@@ -0,0 +1,13 @@
+data = ['ぱんだ', 'うさぎ', 'こあら', 'とら']
+data2 = [205, 13, 78, 50]
+data3 = ['ぱんだ', 15, 'こあら']
+
+data.sort()
+print(data)
+data2.sort()
+print(data2)
+# data3.sort() #エラー
+print(data3)
+
+data.sort(reverse=True)
+print(data)
