025d4e4e01d1460af648000e31a79e7250758348
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-11-01 08:14:31 +0900

    6.1.14 リストの内容を特定の条件で絞り込む
    まで学習完了

---
+++ chap06/list_all.py
@@ -0,0 +1,9 @@
+print(all([False, True, False]))
+print(any([False, True, False]))
+print(not any([False, False, False]))
+
+print(any(['あいう', '', '']))
+print(any(['', '', '']))
+
+data = ['さざんか', 'ほうせんか', 'バラ', 'サクラ']
+print(any([len(str) > 4 for str in data]))
