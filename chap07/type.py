752c74551a51f310861f4dc4436451c3cd25306c
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-11-27 20:05:15 +0900

    chap07 学習完了

---
+++ chap07/type.py
@@ -0,0 +1,22 @@
+print(bool(''))
+print(bool(150))
+
+dec_num = int('10')
+print(dec_num)
+print(type(dec_num))
+
+# i_num = int('1.414')
+# print(i_num)
+# print(type(i_num))
+
+hex_num = int('0x10', 16)
+print(hex_num)
+print(type(hex_num))
+
+f_num = float('1.414e-5')
+print(f_num)
+print(type(f_num))
+
+str_v = str(1.0414)
+print(str_v)
+print(type(str_v))
