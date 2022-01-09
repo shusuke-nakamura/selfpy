b346089b3b0593505120ca3cc0dc0f39d5d6e8cd
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2022-01-09 23:29:49 +0900

    リスト10.10
    まで学習完了

--- chap10/class_method.py
+++ chap10/class_method.py
@@ -3,10 +3,10 @@ class Person:
         self.firstname = firstname
         self.lastname = lastname
 
+    def show(self):
+        print(f'私の名前は{self.lastname}{self.firstname}です！')
+
 
 if __name__ == '__main__':
-    p1 = Person('太郎', '山田')
-    p1.age = 52
-    p2 = Person('花子', '鈴木')
-    print(p1.age)
-    print(p2.age)
+    p = Person('太郎', '山田')
+    p.show()
